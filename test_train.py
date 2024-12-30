df4ae72a04a060d2e62db90fc2508109b8403930
pythonprav <137261430+pythonprav@users.noreply.github.com>
2024-12-30 17:59:07 +0800

    Delete test_train.py

--- test_train.py
+++
@@ -1,9 +0,0 @@
-# tests/test_train.py
-# import pytest
-import joblib
-from train_model import train_model
-
-def test_training():
-    train_model()
-    model = joblib.load("model.pkl")
-    assert model is not None
